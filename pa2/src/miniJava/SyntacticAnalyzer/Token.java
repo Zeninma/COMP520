fd405e42010e50aabd86b98bd6c13d12cfbd59b8
Ao Zeng <13871299198@msn.cn>
2017-02-19 15:48:14 -0500

    Add SyntaxTreePackage

--- pa2/src/miniJava/SyntacticAnalyzer/Token.java
+++ pa2/src/miniJava/SyntacticAnalyzer/Token.java
@@ -9,9 +9,11 @@ import miniJava.SyntacticAnalyzer.TokenKind;
 public class Token {
 	public TokenKind kind;
 	public String spelling;
+	public SourcePosition posn;
 
 	public Token(TokenKind kind, String spelling) {
 		this.kind = kind;
 		this.spelling = spelling;
+		this.posn = null;
 	}
 }
